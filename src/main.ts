10784b03f08a1d2792aa56e58199063366314a8f
Idle <651462+reonZ@users.noreply.github.com>
2022-12-11 19:37:55 +0100

    added bff

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { getCurrentModule } from './@utils/foundry/module'
+import { getSetting } from './@utils/foundry/settings'
 import { esotericCheck } from './macros/esoteric'
 import { identify } from './macros/identify'
 import { manualToken } from './macros/manual'
 import { groupPerception } from './macros/perception'
+import MODULE_ID from './module'
+import { enableBFF } from './modules/bff'
 
 Hooks.once('init', () => {
     getCurrentModule().api = {
@@ -13,4 +16,17 @@ Hooks.once('init', () => {
             identify,
         },
     }
+
+    game.settings.register(MODULE_ID, 'bff', {
+        name: "Enable BFF's Ire",
+        type: Boolean,
+        default: true,
+        config: true,
+        scope: 'world',
+        onChange: enableBFF,
+    })
+})
+
+Hooks.once('ready', () => {
+    if (getSetting('bff')) enableBFF(true)
 })
---
+++ src/modules/bff.ts
@@ -0,0 +1,45 @@
+const effectId = 'Item.pFguo7KVVjFMqHhe'
+
+export function enableBFF(enable: boolean) {
+    if (!game.user.isGM) return
+    const method = enable ? 'on' : 'off'
+    Hooks[method]('updateToken', updateToken)
+}
+
+function getDebuff(actor: ActorPF2e) {
+    return actor.itemTypes.effect.find(x => x.getFlag('core', 'sourceId') === effectId)
+}
+
+function updateToken(token: TokenDocument, data: DocumentUpdateData<TokenDocument>) {
+    const actor = token.actor as ActorPF2e | undefined
+    if (!('x' in data || 'y' in data) || !actor) return
+    const debuff = getDebuff(actor)
+    if (debuff) setDebuff(token, debuff)
+}
+
+function setDebuff(firstToken: TokenDocument, firstDebuff: EffectPF2e) {
+    let otherToken
+    let otherDebuff
+
+    const tokens = canvas.tokens.placeables as TokenPF2e[]
+    for (const token of tokens) {
+        const otherActor = token.actor
+        if (!otherActor || otherActor === firstToken.actor) continue
+        const debuff = getDebuff(otherActor)
+        if (!debuff) continue
+        otherToken = token
+        otherDebuff = debuff
+        break
+    }
+
+    if (!otherToken || !otherDebuff) {
+        firstDebuff.update({ 'system.badge.value': 0 })
+        return
+    }
+
+    const distance = canvas.grid.measureDistance(firstToken, otherToken, { gridSpaces: true })
+    const debuff = Math.max(distance - 5, 0) / 5
+
+    firstDebuff.update({ 'system.badge.value': debuff })
+    otherDebuff.update({ 'system.badge.value': debuff })
+}
