eb21f52ed32e8bb27b2cca6639f9084d2f0d27b2
Idle <651462+reonZ@users.noreply.github.com>
2023-02-22 23:32:39 +0100

    1.8.0

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
-import { MODULE_ID, setModuleID } from '@utils/module'
-import { socketOn } from '@utils/socket'
 import { getCurrentModule } from '@utils/foundry/module'
-import { getSetting } from '@utils/foundry/settings'
-import { enableBFF } from './modules/bff'
-import { exploitVulnerability, exploitVulnerabilityGM } from './macros/exploit-vulnerability'
+import { setModuleID } from '@utils/module'
+import { socketOn } from '@utils/socket'
 import { esotericCheck } from './macros/esoteric'
-import { manualToken } from './macros/manual-token'
-import { groupPerception } from './macros/perception'
+import { exploitVulnerability, exploitVulnerabilityGM } from './macros/exploit-vulnerability'
 import { identify } from './macros/identify'
 import { ripImaginarium } from './macros/imaginarium'
+import { manualToken } from './macros/manual-token'
+import { groupPerception } from './macros/perception'
 
 setModuleID('idleuh')
 
@@ -24,28 +22,28 @@ Hooks.once('init', () => {
         },
     }
 
-    game.settings.register(MODULE_ID, 'bff', {
-        name: "Enable BFF's Ire",
-        hint: "Should the BFF's Ire be handled.",
-        type: Boolean,
-        default: true,
-        config: true,
-        scope: 'world',
-        onChange: enableBFF,
-    })
+    // game.settings.register(MODULE_ID, 'bff', {
+    //     name: "Enable BFF's Ire",
+    //     hint: "Should the BFF's Ire be handled.",
+    //     type: Boolean,
+    //     default: true,
+    //     config: true,
+    //     scope: 'world',
+    //     onChange: enableBFF,
+    // })
 
-    game.settings.register(MODULE_ID, 'bffDistance', {
-        name: "BFF's Ire Distance",
-        hint: 'Distance in square(s) for the curse to apply.',
-        type: Number,
-        default: 1,
-        config: true,
-        scope: 'world',
-    })
+    // game.settings.register(MODULE_ID, 'bffDistance', {
+    //     name: "BFF's Ire Distance",
+    //     hint: 'Distance in square(s) for the curse to apply.',
+    //     type: Number,
+    //     default: 1,
+    //     config: true,
+    //     scope: 'world',
+    // })
 })
 
 Hooks.once('ready', () => {
-    if (getSetting('bff')) enableBFF(true)
+    // if (getSetting('bff')) enableBFF(true)
     if (game.user.isGM) {
         socketOn(onPacketReceived)
     }
