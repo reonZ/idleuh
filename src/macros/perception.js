1dabed3ecf5c50abdac2b7fe542f2402cce17c30
Idle <651462+reonZ@users.noreply.github.com>
2024-02-09 22:10:59 +0100

    1.37.0

--- src/macros/perception.js
+++ src/macros/perception.js
@@ -8,18 +8,19 @@ export async function groupPerception() {
 
     let content = ''
 
-    const party = game.actors.party
-    const actors = party?.members ?? []
+    const actors = game.actors.party?.members ?? []
 
     await Promise.all(
         actors.map(async actor => {
+            if (!actor.isOfType('character')) return
+
             const roll = await actor.perception.roll({ createMessage: false })
             const die = roll.dice[0].total
 
             content += `<div style="display:flex;justify-content:space-between;" title="${roll.result}">`
             content += `<span>${actor.name} (${proficiency[actor.perception.rank]})</span><span`
-            if (die == 20) content += ' style="color: green;"'
-            else if (die == 1) content += ' style="color: red;"'
+            if (die === 20) content += ' style="color: green;"'
+            else if (die === 1) content += ' style="color: red;"'
             content += `>${roll.total}</span></div>`
         })
     )
