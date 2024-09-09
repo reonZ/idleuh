33dd5af12e40375a1f04f116ca6535801f762cf4
Idle <651462+reonZ@users.noreply.github.com>
2024-09-09 13:16:35 +0200

    add settings markers + eat settings input events

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
-import { MODULE, getSetting, registerSetting } from "foundry-pf2e";
+import {
+    MODULE,
+    createHTMLElement,
+    getSetting,
+    htmlClosest,
+    htmlQuery,
+    registerSetting,
+} from "foundry-pf2e";
 import { identify } from "./identify";
 import { ripImaginarium } from "./imaginarium";
 import { groupPerception } from "./perception";
-import { thermalNimbus } from "./thermal-nimbus";
-import { spikeSkinDamage, spikeSkinDuration } from "./spike-skin";
 import { selectVictim } from "./select-victim";
+import { spikeSkinDamage, spikeSkinDuration } from "./spike-skin";
+import { thermalNimbus } from "./thermal-nimbus";
 
 MODULE.register("idleuh", "Idleuh");
 
@@ -43,7 +50,49 @@ Hooks.once("init", () => {
 });
 
 Hooks.once("ready", () => {
-    if (getSetting("jquery")) setJQueryFx(true);
+    if (getSetting("jquery")) {
+        setJQueryFx(true);
+    }
+});
+
+Hooks.on("renderSettingsConfig", (app: SettingsConfig, $html: JQuery, data: SettingsConfigData) => {
+    const html = $html[0];
+
+    const setGroupName = (group: Maybe<HTMLElement>, scope: "client" | "world") => {
+        const label = htmlQuery(group, ":scope > label");
+        const icon = createHTMLElement("span", {
+            dataset: {
+                tooltip: scope.capitalize(),
+                tooltipDirection: "UP",
+            },
+            innerHTML: scope === "world" ? "🌎 " : "👤 ",
+        });
+        label?.prepend(icon);
+    };
+
+    for (const category of data.categories) {
+        const section = htmlQuery(html, `section.category[data-category="${category.id}"]`);
+        if (!section) continue;
+
+        for (const setting of category.settings) {
+            const group = htmlQuery(section, `[data-setting-id="${setting.id}"]`);
+            setGroupName(group, setting.scope);
+        }
+
+        for (const menu of category.menus) {
+            const btn = htmlQuery(section, `[data-key="${menu.key}"]`);
+            const group = htmlClosest(btn, ".form-group");
+            setGroupName(group, menu.restricted ? "world" : "client");
+        }
+    }
+
+    const inputs = html.querySelectorAll("input[type='range'], input[type='number']");
+    for (const input of inputs) {
+        input.addEventListener("wheel", (event) => {
+            event.preventDefault();
+            event.stopPropagation();
+        });
+    }
 });
 
 function setJQueryFx(disabled: boolean) {
